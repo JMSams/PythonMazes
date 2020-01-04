f49049917b90c03241bca680789125876fc876ce
Jamie Sams <jsams@fallingsloth.uk>
2020-01-04 15:19:28 +0000

    Sidewinder implemented.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from random import randint
 
 from options import *
 
-if ALGORITHM == 0 or ALGORITHM < 0 or ALGORITHM > 2:
+if ALGORITHM == 0 or ALGORITHM < 0 or ALGORITHM > ALGORITHM_COUNT:
 	ALGORITHM = randint(1, 2)
 if ALGORITHM == 1:
 	from BinaryTree import BinaryTree as Algorithm
@@ -23,6 +23,9 @@ if ALGORITHM == 1:
 elif ALGORITHM == 2:
 	from RecursiveBacktracker import RecursiveBacktracker as Algorithm
 	AlgorithmName = "Recursive Backtracker"
+elif ALGORITHM == 3:
+	from Sidewinder import Sidewinder as Algorithm
+	AlgorithmName = "Sidewinder"
 
 rows = GRID_SIZE_Y if isinstance(GRID_SIZE_Y, int) else randint(GRID_SIZE_Y[0], GRID_SIZE_Y[1])
 cols = GRID_SIZE_X if isinstance(GRID_SIZE_X, int) else randint(GRID_SIZE_X[0], GRID_SIZE_X[1])
--- options.py
+++ options.py
@@ -1,11 +1,13 @@
 GRID_COLOUR = "cyan"
 SHOW_COORDS = False
 
-GRID_SIZE_X = 15
-GRID_SIZE_Y = 17
+GRID_SIZE_X = 5
+GRID_SIZE_Y = 5
 
 # Algorithms:
 # 0: random
 # 1: binary tree
 # 2: recursive backtracker
+# 3: sidewinder
 ALGORITHM = 0
+ALGORITHM_COUNT = 3
