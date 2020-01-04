266993fa13c7507223779b1a753cf73679ca46d6
Jamie Sams <jsams@fallingsloth.uk>
2020-01-04 14:32:48 +0000

    Main now shows algorithm name, out-of-range algorithm indexes are now treated as 0

--- main.py
+++ main.py
@@ -15,17 +15,18 @@ from random import randint
 
 from options import *
 
-if ALGORITHM == 0:
+if ALGORITHM == 0 or ALGORITHM < 0 or ALGORITHM > 2:
 	ALGORITHM = randint(1, 2)
 if ALGORITHM == 1:
 	from BinaryTree import BinaryTree as Algorithm
+	AlgorithmName = "Binary Tree"
 elif ALGORITHM == 2:
 	from RecursiveBacktracker import RecursiveBacktracker as Algorithm
+	AlgorithmName = "Recursive Backtracker"
 
 rows = GRID_SIZE_Y if isinstance(GRID_SIZE_Y, int) else randint(GRID_SIZE_Y[0], GRID_SIZE_Y[1])
 cols = GRID_SIZE_X if isinstance(GRID_SIZE_X, int) else randint(GRID_SIZE_X[0], GRID_SIZE_X[1])
 themaze = Algorithm(rows, cols)
 
-print("rows: " + str(themaze.rows))
-print("cols: " + str(themaze.columns))
+print("Algorithm: " + AlgorithmName)
 print(themaze)
