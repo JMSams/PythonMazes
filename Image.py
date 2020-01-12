12f6852f13cbaf2af7ee8002efffc4f8db2a3475
Jamie Sams <jsams@fallingsloth.uk>
2020-01-12 03:21:51 +0000

    Image output now working!

--- Image.py
+++ Image.py
@@ -28,7 +28,29 @@ def SelectSprite(cell):
 		return Sprite_NES
 	elif cell.IsLinked([cell.north, cell.south, cell.west]):
 		return Sprite_NSW
-	elif cell.IsLinked([cell.north, cell.south, cell.west]):
-		return Sprite_NSW
+	elif cell.IsLinked([cell.north, cell.east, cell.west]):
+		return Sprite_NEW
+	elif cell.IsLinked([cell.south, cell.east, cell.west]):
+		return Sprite_SEW
+	elif cell.IsLinked([cell.north, cell.east]):
+		return Sprite_NE
+	elif cell.IsLinked([cell.north, cell.west]):
+		return Sprite_NW
+	elif cell.IsLinked([cell.south, cell.east]):
+		return Sprite_SE
+	elif cell.IsLinked([cell.south, cell.west]):
+		return Sprite_SW
+	elif cell.IsLinked([cell.north, cell.south]):
+		return Sprite_NS
+	elif cell.IsLinked([cell.east, cell.west]):
+		return Sprite_EW
+	elif cell.IsLinked([cell.north]):
+		return Sprite_N
+	elif cell.IsLinked([cell.east]):
+		return Sprite_E
+	elif cell.IsLinked([cell.south]):
+		return Sprite_S
+	elif cell.IsLinked([cell.west]):
+		return Sprite_W
 	else:
 		return Sprite__
